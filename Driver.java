fad2905e572c8c04bef588012ad4e4be761e337e
Brandon Chao <bchao1@stuy.edu>
2019-03-12 23:14:19 -0400

    quickselect and randomness is not helping

---
+++ Driver.java
@@ -0,0 +1,24 @@
+public class Driver {
+
+  public static void main(String[] args) {
+    int[]ary = { 2, 10, 15, 23, 0,  5};//, 12,21,212,123};  //sorted :  {0,2,5,10,12,15,21,23,123,212}
+
+System.out.println(Quick.quickselect( ary , 0 )); //would return 0
+System.out.println(Quick.quickselect( ary , 1 ));  //would return 2
+System.out.println(Quick.quickselect( ary , 2 ));  //would return 5
+System.out.println(Quick.quickselect( ary , 3 ));  //would return 10
+System.out.println(Quick.quickselect( ary , 4 ));  //would return 12
+System.out.println(Quick.quickselect( ary , 5 ));  //would return 15
+/*
+System.out.println(Quick.quickselect( ary , 6 ));  //would return 21
+System.out.println(Quick.quickselect( ary , 7 ));  //would return 23
+System.out.println(Quick.quickselect( ary , 8 ));  //would return 123
+System.out.println(Quick.quickselect( ary , 9 ));  //would return 212
+*/
+String me = "";
+for (int x = 0; x < ary.length;x++) {
+  me += ary[x] + " ";
+}
+System.out.println(me);
+  }
+}
--- Quick.java
+++ Quick.java
@@ -5,19 +5,17 @@ public class Quick{
 /*return the value that is the kth smallest value of the array. k=0 is the smallest
 */
 public static int quickselect(int[] data, int k) {
-   int index = k;
-   int s = 0;
-   int e = data.length;
-   int returnIndex = partition(data,s,e);
-   while (index != returnIndex) {
-     if (index > returnIndex) {
-       returnIndex = partition(data,returnIndex,e);
+   int returnIndex = partition(data,0,data.length-1);
+   while (k != returnIndex) {
+     //System.out.println("r" + returnIndex);
+     if (k < returnIndex) {
+       returnIndex = partition(data,0,returnIndex);
      }
      else {
-       returnIndex = partition(data,s,returnIndex);
+       returnIndex = partition(data,returnIndex,data.length-1);
      }
    }
-   return data[returnIndex];
+   return data[k];
 }
 
 /*Modify the array to be in increasing order.
@@ -37,33 +35,32 @@ public static int partition (int [] data, int start, int end) {
     storage = data[pivot];
     data[pivot] = data[starter];
     data[starter] = storage;
-
-    for (int x = starter+1; x < data.length ; x++) {
-      if (x == ender) {
-        if (data[x] <= data[starter]) {
-          storage = data[x];
-          data[x] = data[starter];
-          data[starter] = storage;
-          return x;
-        }
-        else {
-          storage = data[x-1];
-          data[x-1] = data[starter];
-          data[starter] = storage;
-          return x-1;
-        }
-
+    start++;
+    while (start != end) {
+      if (data[start] > data[starter]) {
+        storage = data[start];
+        data[start] = data[end];
+        data[end] = storage;
+        end--;
       }
-      if (data[x] > data[starter]) {
-        storage = data[ender];
-        data[ender] = data[x];
-        data[x] = storage;
-        ender--;
-        x--;
+      else {
+        start++;
       }
-
     }
-    return -1;
+
+    if (data[start] < data[starter]) {
+      storage = data[start];
+      data[start] = data[starter];
+      data[starter] = storage;
+      return start;
+    }
+    else {
+      storage = data[start-1];
+      data[start-1] = data[starter];
+      data[starter] = storage;
+      return start-1;
+    }
+
   }
 
 }
